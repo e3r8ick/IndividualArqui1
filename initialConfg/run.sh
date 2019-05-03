1a54e215c885dc24b6e05962e0179adb1b3d1457
Erick Cordero <eguicoro2@gmail.com>
2019-05-02 19:47:10 -0600

    run changed to arm architecture

--- initialConfg/run.sh
+++ initialConfg/run.sh
@@ -1,23 +1,15 @@
-#!/usr/bin/env bash
+sudo apt-get install cloud-image-utils qemu-system-arm qemu-efi
 
-sudo apt-get install cloud-image-utils qemu
-
-# This is already in qcow2 format.
-img=ubuntu-18.04-server-cloudimg-amd64.img
+# Get the image.
+img=ubuntu-18.04-server-cloudimg-arm64.img
 if [ ! -f "$img" ]; then
   wget "https://cloud-images.ubuntu.com/releases/18.04/release/${img}"
-
-  # sparse resize: does not use any extra space, just allows the resize to happen later on.
-  # https://superuser.com/questions/1022019/how-to-increase-size-of-an-ubuntu-cloud-image
   qemu-img resize "$img" +128G
 fi
 
+# For the password.
 user_data=user-data.img
 if [ ! -f "$user_data" ]; then
-  # For the password.
-  # https://stackoverflow.com/questions/29137679/login-credentials-of-ubuntu-cloud-server-image/53373376#53373376
-  # https://serverfault.com/questions/920117/how-do-i-set-a-password-on-an-ubuntu-cloud-image/940686#940686
-  # https://askubuntu.com/questions/507345/how-to-set-a-password-for-ubuntu-cloud-images-ie-not-use-ssh/1094189#1094189
   cat >user-data <<EOF
 #cloud-config
 password: asdfqwer
@@ -25,16 +17,25 @@ chpasswd: { expire: False }
 ssh_pwauth: True
 EOF
   cloud-localds "$user_data" user-data
+
+  # Use the EFI magic. Picked up from:
+  # https://wiki.ubuntu.com/ARM64/QEMU
+  dd if=/dev/zero of=flash0.img bs=1M count=64
+  dd if=/usr/share/qemu-efi/QEMU_EFI.fd of=flash0.img conv=notrunc
+  dd if=/dev/zero of=flash1.img bs=1M count=64
 fi
 
-qemu-system-x86_64 \
-  -drive "file=${img},format=qcow2" \
-  -drive "file=${user_data},format=raw" \
+qemu-system-aarch64 \
+  -M virt \
+  -cpu cortex-a57 \
   -device rtl8139,netdev=net0 \
-  -enable-kvm \
-  -m 2G \
+  -m 4096 \
   -netdev user,id=net0 \
-  -serial mon:stdio \
-  -smp 2 \
-  -vga virtio \
-  ;
+  -nographic \
+  -smp 4 \
+  -drive "if=none,file=${img},id=hd0" \
+  -device virtio-blk-device,drive=hd0 \
+  -drive "file=${user_data},format=raw" \
+  -pflash flash0.img \
+  -pflash flash1.img \
+;
