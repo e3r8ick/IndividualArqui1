c3e53e93baacecb4bc67076bcb3e68c11867c802
Erick Cordero <eguicoro2@gmail.com>
2019-05-19 16:57:36 -0600

    wef

--- TestCode/RSA.s
+++ TestCode/RSA.s
@@ -4,14 +4,14 @@ __main:
 	BL ITSPRIME
 
 ITSPRIME:
-	 MOV R0,R4               @Number to check
+	 MOV R0,               @Number to check
 	 CMP R0,#01               @Comparing with 01
 	 BEQ PRIME                @If equal => prime
 	 CMP R0,#02               @Compare with 02
 	 BEQ PRIME                @If equal => prime
 	 MOV R1,R0                @Copy the number in R1
 	 MOV R2,#02               @Initial divider
-	 
+
 UP:                     
 	 BL DIVISION              @Call for division sub-function
 	 CMP R8,#00               @Compare remainder with 0
@@ -24,13 +24,16 @@ UP:
 NOTPRIME:
 	 MOV R3,#0                @The number is not prime
 	 B STOP                             @Jump STOP
+
 PRIME:
 	MOV R3,#1                 @The number is prime
+	
+STOP:
+	B EXIT                             @Jump EXIT
  
 DIVISION:                         @Function for division operation
 	 MOV R8,R0                @Copy of data from main function
 	 MOV R9,R2                @Copy of divider from main function
-
 LOOP:              
 	 SUB R8,R8,R9              @Successive subtraction for division
 	 ADD R10,R10,#01        @Counter for holding the result of division
