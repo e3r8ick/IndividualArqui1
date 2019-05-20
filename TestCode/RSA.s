7ec51c8b431a07bea4aa88b929f7364c5b8fc2a4
Erick Cordero <eguicoro2@gmail.com>
2019-05-20 15:40:04 -0600

    dsds

--- TestCode/RSA.s
+++ TestCode/RSA.s
@@ -1,98 +1,104 @@
+.global __main
+.func __main
+
 __main:
-	MOV R11, #17			@Key pair number1
-	MOV R12, #29            @keY pair number2
+	MOV R11, #17			@ Key pair number1
+	MOV R12, #29            @ keY pair number2
 
 ITSPRIME_A:
-	MOV R0,R11				@Move the first number to check
-	CMP R0,#01             	@Comparing with 01
-	BEQ PRIME_A             @If equal => prime
-	CMP R0,#02             	@Compare with 02
-	BEQ PRIME_A             @If equal => prime
-	MOV R1,R0              	@Copy the number in R1
-	MOV R2,#02             	@Initial divider
+	MOV R0,R11				@ Move the first number to check
+	CMP R0,#01             	@ Comparing with 01
+	BEQ PRIME_A             @ If equal => prime
+	CMP R0,#02             	@ Compare with 02
+	BEQ PRIME_A             @ If equal => prime
+	MOV R1,R0              	@ Copy the number in R1
+	MOV R2,#02             	@ Initial divider
 
 UP_A:                     
-	BL DIVISION_A           @Call for division sub-function
-	CMP R8,#00             	@Compare remainder with 0
-	BEQ NOTPRIME_A        	@If equal => not prime
-	ADD R2,R2,#01          	@If not increment divider and check
-	CMP R2,R1              	@Compare divider with the number
-	BEQ PRIME_A            	@All possible numbers are done => prime
-	B UP_A                 	@If not repeat until end
+	BL DIVISION_A           @ Call for division sub-function
+	CMP R8,#00             	@ Compare remainder with 0
+	BEQ NOTPRIME_A        	@ If equal => not prime
+	ADD R2,R2,#01          	@ If not increment divider and check
+	CMP R2,R1              	@ Compare divider with the number
+	BEQ PRIME_A            	@ All possible numbers are done => prime
+	B UP_A                 	@ If not repeat until end
 
 NOTPRIME_A:
-	MOV R3,#0       		@The number is not prime
-	B ITSPRIME_B           	@Jump ITSPRIME_B
+	MOV R3,#0       		@ The number is not prime
+	B ITSPRIME_B           	@ Jump ITSPRIME_B
 
 PRIME_A:
-	MOV R3,#1       		@The number is prime
-	B ITSPRIME_B            @Jump ITSPRIME_B
+	MOV R3,#1       		@ The number is prime
+	B ITSPRIME_B            @ Jump ITSPRIME_B
  
-DIVISION_A:                 @Function for division operation
-	MOV R8,R0              	@Copy of data from main function
-	MOV R9,R2              	@Copy of divider from main function
+DIVISION_A:                 @ Function for division operation
+	MOV R8,R0              	@ Copy of data from main function
+	MOV R9,R2              	@ Copy of divider from main function
 	 
 LOOP_A:              
-	SUB R8,R8,R9           	@Successive subtraction for division
-	ADD R10,R10,#01        	@Counter for holding the result of division
-	CMP R8,R9              	@Compares for non-zero result
-	BPL LOOP_A              @Repeats the loop if subtraction is still needed
-	MOV PC,LR              	@Return back to main function
+	SUB R8,R8,R9           	@ Successive subtraction for division
+	ADD R10,R10,#01        	@ Counter for holding the result of division
+	CMP R8,R9              	@ Compares for non-zero result
+	BPL LOOP_A              @ Repeats the loop if subtraction is still needed
+	MOV PC,LR              	@ Return back to main function
 
 ITSPRIME_B:
-	MOV R0,R12				@Move the second number to check
-	CMP R0,#01             	@Comparing with 01
-	BEQ PRIME_B             @If equal => prime
-	CMP R0,#02            	@Compare with 02
-	BEQ PRIME_B             @If equal => prime
-	MOV R1,R0              	@Copy the number in R1
-	MOV R2,#02             	@Initial divider
+	MOV R0,R12				@ Move the second number to check
+	CMP R0,#01             	@ Comparing with 01
+	BEQ PRIME_B             @ If equal => prime
+	CMP R0,#02            	@ Compare with 02
+	BEQ PRIME_B             @ If equal => prime
+	MOV R1,R0              	@ Copy the number in R1
+	MOV R2,#02             	@ Initial divider
 
 UP_B:                     
-	BL DIVISION_B           @Call for division sub-function
-	CMP R8,#00             	@Compare remainder with 0
-	BEQ NOTPRIME_B        	@If equal => not prime
-	ADD R2,R2,#01          	@If not increment divider and check
-	CMP R2,R1              	@Compare divider with the number
-	BEQ PRIME_B            	@All possible numbers are done => prime
-	B UP_B                 	@If not repeat until end
+	BL DIVISION_B           @ Call for division sub-function
+	CMP R8,#00             	@ Compare remainder with 0
+	BEQ NOTPRIME_B        	@ If equal => not prime
+	ADD R2,R2,#01          	@ If not increment divider and check
+	CMP R2,R1              	@ Compare divider with the number
+	BEQ PRIME_B            	@ All possible numbers are done => prime
+	B UP_B                 	@ If not repeat until end
 
 NOTPRIME_B:
-	MOV R4,#0       		@The number is not prime
-	B CHECK_PRIME_A        	@Jump CHECK_PRIME_A
+	MOV R4,#0       		@ The number is not prime
+	B CHECK_PRIME_A        	@ Jump CHECK_PRIME_A
 
 PRIME_B:
-	MOV R4,#1       		@The number is prime
-	B CHECK_PRIME_A        	@Jump CHECK_PRIME_A
+	MOV R4,#1       		@ The number is prime
+	B CHECK_PRIME_A        	@ Jump CHECK_PRIME_A
  
-DIVISION_B:                 @Function for division operation
-	MOV R8,R0              	@Copy of data from main function
-	MOV R9,R2              	@Copy of divider from main function
+DIVISION_B:                 @ Function for division operation
+	MOV R8,R0              	@ Copy of data from main function
+	MOV R9,R2              	@ Copy of divider from main function
 	 
 LOOP_B:              
-	SUB R8,R8,R9           	@Successive subtraction for division
-	ADD R10,R10,#01        	@Counter for holding the result of division
-	CMP R8,R9              	@Compares for non-zero result
-	BPL LOOP_B              @Repeats the loop if subtraction is still needed
-	MOV PC,LR              	@Return back to main function
+	SUB R8,R8,R9           	@ Successive subtraction for division
+	ADD R10,R10,#01        	@ Counter for holding the result of division
+	CMP R8,R9              	@ Compares for non-zero result
+	BPL LOOP_B              @ Repeats the loop if subtraction is still needed
+	MOV PC,LR              	@ Return back to main function
 
 CHECK_PRIME_A:
-	CMP R3,#1               @To check if the first number was prime
-	BEQ CHECK_PRIME_B       @If equal => prime
-	B EXIT					@If not, exit (sistem alert)
+	CMP R3,#1               @ To check if the first number was prime
+	BEQ CHECK_PRIME_B       @ If equal => prime
+	B EXIT					@ If not, exit (sistem alert)
 
 CHECK_PRIME_B:
-	CMP R4,#1               @To check if the second number was prime
-	BEQ CHECK_EQUAL		    @If equal => prime
-	B EXIT					@If not, exit (sistem alert)
+	CMP R4,#1               @ To check if the second number was prime
+	BEQ CHECK_EQUAL		    @ If equal => prime
+	B EXIT					@ If not, exit (sistem alert)
 
 CHECK_EQUAL:
-	CMP R11,R12             @To check if the numbers are the same
-	BEQ EXIT			    @If equal => EXIT (sistem alert)
-	B GENERATOR				@Jump to the function how generates the keypair
+	CMP R11,R12             @ To check if the numbers are the same
+	BEQ EXIT			    @ If equal => EXIT (sistem alert)
+	B GENERATOR				@ Jump to the function how generates the keypair
 
 GENERATOR:
 	MUL R0,R11,R12			@ n = p*q
+	SUB R1,R11,#1			@ p - 1
+	SUB R2,R12,#1			@ q - 1
+	MUL R3,R1,R2			@ L = (p-1)*(q-1)
 
 EXIT:						@Finish
 
--- TestCode/test.s
+++ TestCode/test.s
@@ -1,40 +1,68 @@
-__main:
-	 MOV R0,#12             @Number to check
-	 CMP R0,#01             @Comparing with 0
-	 BEQ PRIME              @If equal => prime
-	 CMP R0,#02             @Compare with 02
-	 BEQ PRIME              @If equal => prime
-	 MOV R1,R0              @Copy the number in R1
-	 MOV R2,#02             @Initial divider
+/******************************************************************************
+* @file array.s
+* @brief simple array declaration and iteration example
+*
+* Simple example of declaring a fixed-width array and traversing over the
+* elements for printing.
+*
+* @author Christopher D. McMurrough
+******************************************************************************/
+ 
+.global main
+.func main
+   
+main:
+    MOV R0, #0              @ initialze index variable
+writeloop:
+    CMP R0, #100            @ check to see if we are done iterating
+    BEQ writedone           @ exit loop if done
+    LDR R1, =a              @ get address of a
+    LSL R2, R0, #2          @ multiply index*4 to get array offset
+    ADD R2, R1, R2          @ R2 now has the element address
+    STR R2, [R2]            @ write the address of a[i] to a[i]
+    ADD R0, R0, #1          @ increment index
+    B   writeloop           @ branch to next loop iteration
+writedone:
+    MOV R0, #0              @ initialze index variable
+readloop:
+    CMP R0, #100            @ check to see if we are done iterating
+    BEQ readdone            @ exit loop if done
+    LDR R1, =a              @ get address of a
+    LSL R2, R0, #2          @ multiply index*4 to get array offset
+    ADD R2, R1, R2          @ R2 now has the element address
+    LDR R1, [R2]            @ read the array at address 
+    PUSH {R0}               @ backup register before printf
+    PUSH {R1}               @ backup register before printf
+    PUSH {R2}               @ backup register before printf
+    MOV R2, R1              @ move array value to R2 for printf
+    MOV R1, R0              @ move array index to R1 for printf
+    BL  _printf             @ branch to print procedure with return
+    POP {R2}                @ restore register
+    POP {R1}                @ restore register
+    POP {R0}                @ restore register
+    ADD R0, R0, #1          @ increment index
+    B   readloop            @ branch to next loop iteration
+readdone:
+    B _exit                 @ exit if done
+    
+_exit:  
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #21             @ print string length
+    LDR R1, =exit_str       @ string at label exit_str:
+    SWI 0                   @ execute syscall
+    MOV R7, #1              @ terminate syscall, 1
+    SWI 0                   @ execute syscall
+       
+_printf:
+    PUSH {LR}               @ store the return address
+    LDR R0, =printf_str     @ R0 contains formatted string address
+    BL printf               @ call printf
+    POP {PC}                @ restore the stack pointer and return
+   
+.data
 
-UP:                     
-	 BL DIVISION            @Call for division sub-function
-	 CMP R8,#00             @Compare remainder with 0
-	 BEQ NOTPRIME           @If equal => not prime
-	 ADD R2,R2,#01          @If not increment divider and check
-	 CMP R2,R1              @Compare divider with the number
-	 BEQ PRIME              @All possible numbers are done => prime
-	 B UP                   @If not repeat until end
-
-NOTPRIME: 
-	 MOV R3,#0              @The number is not prime
-	 B STOP                 @Jump STOP
-
-PRIME: 
-	 MOV R3,#1              @The number is prime
-
-STOP:
-	B EXIT                  @Jump EXIT
-
-DIVISION:                   @Function for division operation
-	 MOV R8,R0              @Copy of data from main function
-	 MOV R9,R2              @Copy of divider from main function
-
-LOOP:                     
-	 SUB R8,R8,R9           @Successive subtraction for division
-	 ADD R10,R10,#01        @Counter for holding the result of division
-	 CMP R8,R9              @Compares for non-zero result
-	 BPL LOOP               @Repeats the loop if subtraction is still needed
-	 MOV PC,LR              @Return back to main function
-
-EXIT:
+.balign 4
+a:              .skip       400
+printf_str:     .asciz      "a[%d] = %d\n"
+exit_str:       .ascii      "Terminating program.\n"
