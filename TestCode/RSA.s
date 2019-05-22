21b35e733525e32528ed1661185aa6a52bd424c1
Erick Cordero <eguicoro2@gmail.com>
2019-05-22 17:40:12 -0600

    werwer

--- TestCode/RSA.s
+++ TestCode/RSA.s
@@ -111,11 +111,11 @@ ARRAY_GENERATOR:
 	LDR R7,=adr_array_bet	@ load the direction
 	LDR R7,[R7]				@ load the element
 	STR R4,[R7,R6]			@ save the element array
+	LDR R9,[R7,R6]			@ Debug print
 	ADD R4,R4,#1			@ R4++
 	ADD R6,R6,#4			@ Next direction
 	SUB R5,R5,#1			@ counter --
 	CMP R5,#0				@ R5 = 0?
-	LDR R9,[R7,R6]			@ Debug print
 	BNE ARRAY_GENERATOR		@ loop for create the array
 
 EXIT:						@Finish
