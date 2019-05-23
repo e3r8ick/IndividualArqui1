2cf0bedd201a497338f471c2710a6dd9f68104f3
Erick Cordero <eguicoro2@gmail.com>
2019-05-22 23:18:25 -0600

    zsw

--- TestCode/test.s
+++ TestCode/test.s
@@ -3,7 +3,7 @@
 gcd:
     mov r0, #17
     mov r1, #29
-     cmp    r0, r1     
-     subgt  r0, r0, r1 
-     sublt  r1, r1, r0 
-     bne    gcd        
+    cmp r0, r1     
+    subgt r0, r0, r1 
+    sublt r1, r1, r0 
+    bne gcd        
