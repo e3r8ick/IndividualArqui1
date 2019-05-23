58cf2591d50a85c6211d61e3509ccd8079d791cf
Erick Cordero <eguicoro2@gmail.com>
2019-05-22 22:11:29 -0600

    dwwd

--- TestCode/test.s
+++ TestCode/test.s
@@ -1,9 +1,9 @@
 .text
-.global main
-main:
-
-        ldr r1, =string
-        svc #0
-
-.data
-string: .asciz "Hello World\n"
+.global gcd
+gcd:
+    mov r0, #17
+    mov r29, #17
+     cmp    r0, r1     
+     subgt  r0, r0, r1 
+     sublt  r1, r1, r0 
+     bne    gcd        
