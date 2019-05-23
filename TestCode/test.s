821fdb150bc701871bd0d713bbe1527ba51f485b
Erick Cordero <eguicoro2@gmail.com>
2019-05-22 22:08:03 -0600

    qwd

--- TestCode/test.s
+++ TestCode/test.s
@@ -1,15 +1,9 @@
 .text
 .global main
 main:
-        push {r7, lr}
 
-        mov r0, #1
         ldr r1, =string
-        mov r2, #12
-        mov r7, #4
         svc #0
-
-        pop {r7, pc}
 
 .data
 string: .asciz "Hello World\n"
