0bd8cd69c67adfa6746c5ed42828aa40ffe7da7d
Erick Cordero <eguicoro2@gmail.com>
2019-05-24 15:29:02 -0600

    rhrt

--- FinalCode/test.s
+++ FinalCode/test.s
@@ -2,12 +2,13 @@
 .global pow
 POW:
     MOV R0,#2           @ number to pow
+    MOV R3,#2           @ number to pow
     MOV R1,#3           @ the pow
     CMP R2,#1           @ if pow = 1
     BEQ NEXT
 
 POW_AUX:
-    MUL R0,R0,R0        @ x*x
+    MUL R0,R0,R3        @ x*x
     SUB R1,R1,#1        @ i--
     CMP R1,#0           @ i == 0
     BNE POW_AUX         @ loot
