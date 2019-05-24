352039f185bf0697ceb7d28b2bcaba274c3f5b2b
Erick Cordero <eguicoro2@gmail.com>
2019-05-24 15:14:23 -0600

    werw

--- FinalCode/test.s
+++ FinalCode/test.s
@@ -3,14 +3,13 @@
 POW:
     MOV R0,#2           @ number to pow
     MOV R1,#3           @ the pow
-    MOV R2,#3           @ i = pow
     CMP R2,#1           @ if pow = 1
     BEQ NEXT
 
 POW_AUX:
-    MUL R4,R0,R0        @ x*x
-    SUB R2,#1           @ i--
-    CMP R2,#0           @ i == 0
+    MUL R0,R0,R0        @ x*x
+    SUB R1,#1           @ i--
+    CMP R1,#0           @ i == 0
     BNE POW_AUX         @ loot
 
 NEXT:
