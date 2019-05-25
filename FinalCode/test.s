48d96b8353bfcab254405b2f250d328e84fb4c1c
Erick Cordero <eguicoro2@gmail.com>
2019-05-24 21:48:25 -0600

    qwdq

--- FinalCode/test.s
+++ FinalCode/test.s
@@ -2,16 +2,16 @@
      @syscall's arguments
      mov R0, -100
      ldr R1, pfname_addr
-     mov R2, 2
-     mov R8, 56 @syscall R1__NR_openat
+     mov R2, #2
+     mov R8, #56 @syscall R1__NR_openat
      svc #0
      mov R10, R0
 
      ldr R1, p_addr
-     mov R2, 16
-     mov R8, 63 @syscall __NR_read
+     mov R2, #16
+     mov R8, #63 @syscall __NR_read
      svc #0
 
      mov R0, R10
-     mov R8, 57 @syscall __NR_close
+     mov R8, #57 @syscall __NR_close
      svc #0
