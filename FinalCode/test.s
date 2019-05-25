18ab7240c5313962019cfea777761e2fde36a30a
Erick Cordero <eguicoro2@gmail.com>
2019-05-24 21:47:02 -0600

    wef

--- FinalCode/test.s
+++ FinalCode/test.s
@@ -1,17 +1,17 @@
 @openat call
      @syscall's arguments
-     mov x0, -100
-     ldr x1, pfname_addr
-     mov x2, 2
-     mov x8, 56 @syscall x1__NR_openat
+     mov R0, -100
+     ldr R1, pfname_addr
+     mov R2, 2
+     mov R8, 56 @syscall R1__NR_openat
      svc #0
-     mov x10, x0
+     mov R10, R0
 
-     ldr x1, p_addr
-     mov x2, 16
-     mov x8, 63 @syscall __NR_read
+     ldr R1, p_addr
+     mov R2, 16
+     mov R8, 63 @syscall __NR_read
      svc #0
 
-     mov x0, x10
-     mov x8, 57 @syscall __NR_close
+     mov R0, R10
+     mov R8, 57 @syscall __NR_close
      svc #0
