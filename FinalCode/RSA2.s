c2215b0345b86cac12383354df1ee0bbbc4eddd8
Erick Cordero <eguicoro2@gmail.com>
2019-05-24 21:45:48 -0600

    wef

--- FinalCode/RSA2.s
+++ FinalCode/RSA2.s
@@ -6,8 +6,23 @@
 .global __main
 
 __main:
-	MOV R11, #11			@ p
-	MOV R12, #17            @ q
+	MOV R0, #11			   @ e 
+	MOV R1, #17            @ n
+	MOV R2, #17            @ d
+	MOV R3, #17            @ message
+
+POW:
+    MOV R4,R3           	@ number to pow (a)
+    MOV R5,R3           	@ number to pow (a)
+    MOV R6,R0           	@ the pow (b)
+    CMP R6,#1           	@ if pow = 1
+    BEQ DIVISION_MOD
+
+POW_AUX:
+    MUL R4,R5,R4       	 	@ x*x (a^b)
+    SUB R6,R6,#1        	@ i--
+    CMP R6,#1           	@ i == 1
+    BNE POW_AUX         	@ loot
 
 EXIT:						@Finish
 	bkpt
--- FinalCode/test.s
+++ FinalCode/test.s
@@ -1,36 +1,17 @@
-.text
-.global __main
+@openat call
+     @syscall's arguments
+     mov x0, -100
+     ldr x1, pfname_addr
+     mov x2, 2
+     mov x8, 56 @syscall x1__NR_openat
+     svc #0
+     mov x10, x0
 
-__main:
-    ldr R1, =OpenParams @ parameters block for OPEN
-    mov R0, #0x01 @ code number for Open File
-    swi 0x123456 @ open a text file for input
-    cmp R0, #0
-    blt OpenError @ branch if there was an error
-    ldr R1, =ReadParams
-    str R0,[R1] @ save the file handle into
-    @ parameters block for READ
-    mov R0, #0x06 @ code number for Read File
-    swi 0x123456 @ read from the text file
-    cmp R0, #0
-    bne ReadError @ branch if there was an error
+     ldr x1, p_addr
+     mov x2, 16
+     mov x8, 63 @syscall __NR_read
+     svc #0
 
-ReadError:
-
-OpenError:
-
-    .data
-ReadParams:
-    .word 0 @ the file handle
-    .word InputBuffer @ address of input buffer
-    .word 80 @ number of bytes to read
-InputBuffer:
-    .skip 80
-OpenParams:
-    .word FileName
-    .word FileNameEnd-FileName @ length of filename
-    .word 0 @ File mode = read
-FileName:
-    .ascii "params.txt" @ name without final NUL byte
-FileNameEnd:
-    .byte 0 @ the NUL byte
+     mov x0, x10
+     mov x8, 57 @syscall __NR_close
+     svc #0
