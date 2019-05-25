f28495f38dc66e3e935d3f5afae86240c59c5bcd
Erick Cordero <eguicoro2@gmail.com>
2019-05-24 22:06:44 -0600

    rty

--- FinalCode/test.s
+++ FinalCode/test.s
@@ -1,6 +1,7 @@
 .data          /* the .data section is dynamically created and its addresses cannot be easily predicted */
 	array_bet: .word 3  /* variable 1 in memory */
     pfname: .asciz "params.txt"
+    p: .asciz "params.txt"
 @openat call
      @syscall's arguments
      mov R0, #100
@@ -11,7 +12,8 @@
      svc #0
      mov R10, R0
 
-     ldr R1, p_addr
+     ldr R7, =p_addr
+     ldr R1, [R7]
      mov R2, #16
      mov R8, #63 @syscall __NR_read
      svc #0
@@ -20,5 +22,6 @@
      mov R8, #57 @syscall __NR_close
      svc #0
 
-adr_array_bet: .word array_bet  /* address to var1 stored here */
+adr_array_bet: .word p  /* address to var1 stored here */
+p_addr: .word array_bet  /* address to var1 stored here */
 pfname_addr: .word pfname  /* address to var1 stored here */
