939883a86c66652e2b719c322a4509cb26640501
Erick Cordero <eguicoro2@gmail.com>
2019-06-15 09:35:32 -0600

    primo test

--- FinalCode/RSA.s
+++ FinalCode/RSA.s
@@ -6,8 +6,8 @@
 .global __main
 
 __main:
-	MOV R11, #11			@ p
-	MOV R12, #17            @ q
+	MOV R11, #4294967295			@ p
+	MOV R12, #4294967293            @ q
 
 ITSPRIME_A:
 	MOV R0,R11				@ Move the first number to check
